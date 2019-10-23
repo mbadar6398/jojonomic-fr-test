0773340244b8bbe879bf0f591657d2b325efefa5
badar1 <badar@automate.id>
2019-10-23 20:43:50 +0700

    install and add bootstrap into project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import '@/assets/scss/main.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
